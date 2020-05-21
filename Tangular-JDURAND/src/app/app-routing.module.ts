28c294f97ce3b3fea7bede4a421b7b9780cc6fee
Javier Moshé Durand Orellana <javierdurand.webmeister@gmail.com>
2020-05-21 00:29:14 -0500

    implementando css - 2

--- Tangular-JDURAND/src/app/app-routing.module.ts
+++ Tangular-JDURAND/src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { AccesosComponent } from './accesos/accesos.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: LoginComponent },
+  {path: 'accesos', component: AccesosComponent }
+
+];
 
 @NgModule({
+  declarations: [],
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
